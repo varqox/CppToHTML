951401b54e7da68f0f4709429d4ba1bcac9a7135
Krzysztof Małysa <krzyk240@gmail.com>
2013-06-26 18:19:48 +0200

    add pattern, test file and preprocessor

--- main.cpp
+++ main.cpp
@@ -5,9 +5,39 @@
 
 using namespace std;
 
-string code_coloring(const string& code)
+string code_coloring(const string& code) // coloring comments, preprocesor, chars and strings
 {
-	return string("");
+	string ret;
+	for(int cl=code.size(), i=0; i<cl; ++i)
+	{
+		if(code[i]=='#')
+		{
+			ret+="<span class=\"p1\">";
+			while(i<cl && code[i]!='\n')
+			{
+				if(code[i]=='\\' && (i+1<cl && code[i+1]=='\n'))
+				{
+					ret+="\\\n";
+					++i;
+				}
+				else
+				{
+					switch(code[i])
+					{
+						case '<': ret+="&lt;";break;
+						case '>': ret+="&gt;";break;
+						case '&': ret+="&amp;";break;
+						default: ret+=code[i];break;
+					}
+				}
+				++i;
+			}
+			ret+="</span>";
+			--i;
+		}
+		else ret+=code[i];
+	}
+return ret;
 }
 
 int main(int argc, char **argv)
@@ -19,20 +49,33 @@ int main(int argc, char **argv)
 		return 0;
 	}
 	string file_name=argv[1];
-	fstream file(file_name.c_str(), ios_base::in);
+	fstream file(file_name.c_str(), ios_base::in), output;
 	if(!file.good()) cout << "Connot open file!" << endl;
 	else
 	{
+		output.open((file_name+".html").c_str(), ios::out);
 		string input, lol;
-		getline(file, input);
+		int i=2;
+		output << "<html>\n<head>\n<meta charset=\"utf-8\">\n<style>\ndiv:after\n{\n	content: \".\";\n	display: inline-block;\n	clear: both;\n	visibility: hidden;\n	line-height: 0;\n	height: 0;\n}\n\ndiv\n{\n	display: inline-block;\n}\n\nbody\n{\n  background: #272822;\n  color: #f8f8f2;\n  font-family: Helvetica, Arial, sans-serif;\n  font-size: 14px;\n}\n.code\n{\n  width: 40em;\n  border: 2px solid #49483e;\n  border-radius: 4px;\n}\n\n.cpp_code\n{\n  text-align: left;\n	margin: 0;\n  margin-left: 0px;\n  overflow: auto;\n  padding-left: 1em;\n  padding-bottom: 5px;\n  padding-top: 5px;\n  padding-right: 5px;\n  border-left: 2px solid #49483e;\n}\n\n.num_lines\n{\n  color: #8f908a;\n  float: left;\n  margin: 0px;\n  text-align: right;\n  padding-left: 3px;\n  padding-right: 5px;\n  padding-bottom: 5px;\n  padding-top: 5px;\n}\n\n.p1{color: #a6e22e;}\n.p2{color: #ff9b4b;}\n.p3{color: #f92672;}\n.p4{color: #66d9ef;}\n.p5{color: #b8b8b8;}\n.p6{color: #ae81ff;}\n.p7{color: #e6db74;}\n</style>\n</head>\n<body>\n<div class=\"code\">\n<pre class=\"num_lines\">\n";
+		if(!file.eof())
+		{
+			getline(file, input);
+			output << '1' << endl;
+		}
 		while(!file.eof())
 		{
 			input+='\n';
 			getline(file, lol);
 			input+=lol;
+			output << i << "\n";
+			++i;
 		}
 		file.close();
-		cout << input << endl;
+		output << "</pre>\n<pre class=\"cpp_code\">\n";
+		//cout << input << endl;
+		output << code_coloring(input) << endl;
+		output << "</pre>\n</div>\n</body>\n</html>";
+		output.close();
 	}
 return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+#define trol(a,b)\
+cout << a << b
+
+using namespace std;
+typedef long double lli;
+
+class troll
+{
+  vector<int> lol;
+};
+
+void trol()
+{
+  return;
+}
+
+int main()
+{
+	trol(0,1);
+    lli trol=0.0;
+    std::cout << trol <<"\nlol";
+    if(trol==0LL)
+    for(int i=0; i<1; ++i)
+        if(i==0) break;
+    printf("\t%d\n", -1);
+return 0;
+}
