794aea2bd3e9762f906b11b5389479f36b188209
Krzysztof Małysa <krzyk240@gmail.com>
2013-06-28 00:13:58 +0200

    good parser implementation

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ using namespace std;
 p1 - green
 p2 - orange
 p3 - red
-p4 - turquoise
+p4 - turquoise italic
+p41 - turquoise
 p5 - light gray
 p6 - purple
 p7 - yellow
@@ -43,7 +44,7 @@ namespace aho
 			graph[0].fail=graph[0].long_sh_pat=0; // max shorter pattern isn't exist
 		}
 
-		void add_word(const string& word, int id, char color)
+		int add_word(const string& word, int id, char color)
 		{
 			int ver=0; // actual node (vertex)
 			for(int s=word.size(), i=0; i<s; ++i)
@@ -59,6 +60,7 @@ namespace aho
 			graph[ver].pattern_id=id;
 			graph[ver].color=color;
 			graph[ver].depth=word.size();
+		return ver;
 		}
 
 		void add_fails() // and the longest shorter patterns, based on BFS algorithm
@@ -99,13 +101,27 @@ namespace aho
 	}
 
 	vector<int> fin;
+	struct ch_pat
+	{
+		int pos, id;
+		bool is_pattern;
+		ch_pat(int p1, int p2, bool p3): pos(p1), id(p2), is_pattern(p3)
+		{}
+	};
+	queue<ch_pat> troll;
 
-	void find(const string& text)
+	void find(const string& text, int x=0)
 	{
 		vector<int>(text.size()).swap(fin); // clear fin
 		int act=0, pat; // actual node - root
 		for(int s=text.size(), i=0; i<s; ++i)
 		{
+			while(!troll.empty() && troll.front().pos<=x+i-text.size())
+			{
+				tree::graph[troll.front().id].is_pattern=troll.front().is_pattern;
+				troll.pop();
+			}
+			//--------------------------------------
 			while(act>0 && tree::graph[act].E[text[i]]==0)
 				act=tree::graph[act].fail; // while we can't add text[i] to path, go to fail node
 			if(tree::graph[act].E[text[i]]!=0) // if we can add text[i] to path
@@ -121,7 +137,7 @@ namespace aho
 				{
 					if(tree::graph[pat].is_pattern)
 					{
-						fin[i-tree::graph[pat].depth+1]=tree::graph[pat].pattern_id; // add pat node to fin
+						fin[i-tree::graph[pat].depth+1]=pat; // add pat node to fin
 						break;
 					}
 					pat=tree::graph[pat].long_sh_pat; // go to the next pattern
@@ -149,20 +165,20 @@ string to_string(int a)
 return w;
 }
 
-struct pr
+struct change 
 {
-	string str;
+	int pos, id; // pos - position, node id
 	char color;
-	pr(const string& a, char col): str(a), color(col)
+	change(int ps, int idx, char p2): pos(ps), id(idx), color(p2)
 	{}
 };
 bool is_name[256]={}, is_true_name[256]={};
-vector<pr> parse_types;
+queue<change> changs;
 
 void make_tree()
 {
-	vector<aho::tree::node>().swap(aho::tree::graph); // clear tree::graph
-	aho::tree::init(); // initialize tree
+	/*vector<aho::tree::node>().swap(aho::tree::graph); // clear tree::graph
+	aho::tree::init(); // initialize tree*/
 	fstream keys_file("patterns/red_keys.txt", ios::in);
 	string key;
 	while(keys_file.good())
@@ -185,12 +201,6 @@ void make_tree()
 		aho::tree::add_word(key,0,4);
 	}
 	keys_file.close();
-	vector<pr>::iterator i=parse_types.begin();
-	while(i!=parse_types.end())
-	{
-		aho::tree::add_word(i->str,0,i->color);
-		++i;
-	}
 	aho::tree::add_fails(); // add fails edges
 }
 
@@ -200,21 +210,27 @@ void parse(const string& code)
 	aho::tree::init(); // initialize tree
 	aho::tree::add_word("class",0,0);
 	aho::tree::add_word("struct",1,0);
-	aho::tree::add_word("enum",2,0);
-	aho::tree::add_word("namespace",3,0);
-	aho::tree::add_word("typename",4,0);
-	aho::tree::add_word("typedef",5,0);
+	aho::tree::add_word("union",2,0);
+	aho::tree::add_word("enum",3,0);
+	aho::tree::add_word("namespace",4,0);
+	aho::tree::add_word("typename",5,0);
+	aho::tree::add_word("typedef",6,0);
 	aho::tree::add_fails(); // add fails edges
 	aho::find(code);
+	vector<aho::tree::node> pre_graph;
+	pre_graph.swap(aho::tree::graph);
+	aho::tree::init();
 	string type;
+	int ps, id;
 	for(int cl=code.size(), i=0; i<cl; ++i)
 	{
 		if(aho::fin[i]!=0)
 		{
 			type="";
-			switch(aho::tree::graph[aho::fin[i]].pattern_id)
+			switch(pre_graph[aho::fin[i]].pattern_id)
 			{
 				case 0:
+					ps=i;
 					i+=5;
 					while(i<cl && code[i]==' ')
 						++i;
@@ -225,11 +241,14 @@ void parse(const string& code)
 					}
 					if(!type.empty())
 					{
-						parse_types.push_back(pr(type,4));
-						parse_types.push_back(pr(type+"::",2));
+						id=aho::tree::add_word(type,0,4);
+						changs.push(change(ps,id,2));
+						changs.push(change(i,id,4));
+						aho::tree::add_word(type+"::",0,2);
 					}
 					break;
 				case 1:
+					ps=i;
 					i+=6;
 					while(i<cl && code[i]==' ')
 						++i;
@@ -240,11 +259,32 @@ void parse(const string& code)
 					}
 					if(!type.empty())
 					{
-						parse_types.push_back(pr(type,4));
-						parse_types.push_back(pr(type+"::",2));
+						id=aho::tree::add_word(type,0,4);
+						changs.push(change(ps,id,2));
+						changs.push(change(i,id,4));
+						aho::tree::add_word(type+"::",0,2);
 					}
 					break;
 				case 2:
+					ps=i;
+					i+=5;
+					while(i<cl && code[i]==' ')
+						++i;
+					while(i<cl && is_true_name[code[i]])
+					{
+						type+=code[i];
+						++i;
+					}
+					if(!type.empty())
+					{
+						id=aho::tree::add_word(type,0,4);
+						changs.push(change(ps,id,2));
+						changs.push(change(i,id,4));
+						aho::tree::add_word(type+"::",0,2);
+					}
+					break;
+				case 3:
+					ps=i;
 					i+=4;
 					while(i<cl && code[i]==' ')
 						++i;
@@ -254,9 +294,14 @@ void parse(const string& code)
 						++i;
 					}
 					if(!type.empty())
-						parse_types.push_back(pr(type,4));
+					{
+						id=aho::tree::add_word(type,0,4);
+						changs.push(change(ps,id,2));
+						changs.push(change(i,id,4));
+					}
 					break;
-				case 3:
+				case 4:
+					ps=i;
 					i+=9;
 					while(i<cl && code[i]==' ')
 						++i;
@@ -266,9 +311,17 @@ void parse(const string& code)
 						++i;
 					}
 					if(!type.empty())
-						parse_types.push_back(pr(type+"::",2));
+					{
+						id=aho::tree::add_word(type,0,4);
+						aho::troll.push(aho::ch_pat(i-1,id,true));
+						aho::troll.push(aho::ch_pat(i,id,false));
+						changs.push(change(ps,id,2));
+						changs.push(change(i,id,4));
+						id=aho::tree::add_word(type+"::",0,2);
+					}
 					break;
-				case 4:
+				case 5:
+					ps=i;
 					i+=8;
 					while(i<cl && code[i]==' ')
 						++i;
@@ -279,11 +332,14 @@ void parse(const string& code)
 					}
 					if(!type.empty())
 					{
-						parse_types.push_back(pr(type,4));
-						parse_types.push_back(pr(type+"::",2));
+						id=aho::tree::add_word(type,0,4);
+						changs.push(change(ps,id,2));
+						changs.push(change(i,id,4));
+						aho::tree::add_word(type+"::",0,2);
 					}
 					break;
-				case 5:
+				case 6:
+					ps=i;
 					i+=7;
 					while(i<cl && code[i]!=';')
 						++i;
@@ -296,8 +352,10 @@ void parse(const string& code)
 					}
 					if(!type.empty())
 					{
-						parse_types.push_back(pr(type,4));
-						parse_types.push_back(pr(type+"::",2));
+						id=aho::tree::add_word(type,0,4);
+						changs.push(change(ps,id,2));
+						changs.push(change(i,id,4));
+						id=aho::tree::add_word(type+"::",0,2);
 					}
 					break;
 			}
@@ -305,15 +363,27 @@ void parse(const string& code)
 				cout << type << " " << type+"::" << endl;
 		}
 	}
+	make_tree();
 }
 
-string synax_highlight(const string& code)
+void make_changes(int i)
 {
-	aho::find(code);
+	while(!changs.empty() && changs.front().pos<=i)
+	{
+		aho::tree::graph[changs.front().id].color=changs.front().color;
+		changs.pop();
+	}
+}
+
+string synax_highlight(const string& code, int x)
+{
+	aho::find(code, x);
 	string ret;
 	char old_color=0;
 	for(int cl=code.size(), i=0; i<cl; ++i)
 	{
+		make_changes(x+i-code.size());
+		//cout << i << ": " << aho::tree::graph[3].color+0 << endl;
 		//if pattern isn't included in any name (front)
 		if(aho::fin[i]!=0 && (aho::tree::graph[aho::fin[i]].color==5 || aho::tree::graph[aho::fin[i]].color==2 || i==0 || !is_name[code[i-1]]))
 		{
@@ -342,12 +412,13 @@ string synax_highlight(const string& code)
 					if(old_color==4)
 						ret.insert(h+1, "<span class=\"p1\">");
 					else
-						ret.insert(h+1, "<span class=\"p4\">");
+						ret.insert(h+1, "<span class=\"p41\">");
 				}
 				ret+="</span>";
 			}
 			else if(aho::tree::graph[aho::fin[i]].color!=5 || (code[i]!='*' && code[i]!='&'))
 				old_color=aho::tree::graph[aho::fin[i]].color;
+			//cout << aho::fin[i] << "{ " << aho::tree::graph[aho::fin[i]].color+0 << endl;
 			ret+="<span class=\"p"+to_string(aho::tree::graph[aho::fin[i]].color)+"\">";
 			for(; i<end; ++i)
 			{
@@ -379,13 +450,12 @@ return ret;
 string code_coloring(const string& code) // coloring comments, preprocesor, chars and strings
 {
 	parse(code);
-	make_tree();
 	string ret, rest;
 	for(int cl=code.size(), i=0; i<cl; ++i)
 	{
 		if(code[i]=='#') // preprocessor
 		{
-			ret+=synax_highlight(rest);
+			ret+=synax_highlight(rest, i);
 			rest="";
 			ret+="<span class=\"p1\">";
 			while(i<cl && code[i]!='\n')
@@ -412,7 +482,7 @@ string code_coloring(const string& code) // coloring comments, preprocesor, char
 		}
 		else if(i+1<cl && code[i]=='/' && code[i+1]=='/') // oneline comment
 		{
-			ret+=synax_highlight(rest);
+			ret+=synax_highlight(rest, i);
 			rest="";
 			ret+="<span class=\"p8\">";
 			while(i<cl && code[i]!='\n')
@@ -425,7 +495,7 @@ string code_coloring(const string& code) // coloring comments, preprocesor, char
 		}
 		else if(i+1<cl && code[i]=='/' && code[i+1]=='*') // multiline comment
 		{
-			ret+=synax_highlight(rest);
+			ret+=synax_highlight(rest, i);
 			rest="";
 			ret+="<span class=\"p8\">/*";
 			i+=2;
@@ -440,7 +510,7 @@ string code_coloring(const string& code) // coloring comments, preprocesor, char
 		}
 		else if(code[i]=='"' || code[i]=='\'') // strings and chars
 		{
-			ret+=synax_highlight(rest);
+			ret+=synax_highlight(rest, i);
 			rest="";
 			char str_or_char=code[i];
 			ret+="<span class=\"p7\">";
@@ -485,7 +555,7 @@ string code_coloring(const string& code) // coloring comments, preprocesor, char
 				rest+=code[i];
 			else*/
 			{
-				ret+=synax_highlight(rest);
+				ret+=synax_highlight(rest, i);
 				rest="";
 				 bool point=false;
 				 ret+="<span class=\"p6\">";
@@ -517,7 +587,7 @@ string code_coloring(const string& code) // coloring comments, preprocesor, char
 		}
 		else rest+=code[i];
 	}
-	ret+=synax_highlight(rest);
+	ret+=synax_highlight(rest, code.size());
 return ret;
 }
 
@@ -601,7 +671,7 @@ int main(int argc, char **argv)
 		output.open((file_name+".html").c_str(), ios::out);
 		string input, lol;
 		int i=2;
-		output << "<html>\n<head>\n<meta charset=\"utf-8\">\n<style>\ndiv:after\n{\n	content: \".\";\n	display: inline-block;\n	clear: both;\n	visibility: hidden;\n	line-height: 0;\n	height: 0;\n}\n\ndiv\n{\n	display: inline-block;\n}\n\nbody\n{\n  background: #272822;\n  color: #f8f8f2;\n  font-family: Helvetica, Arial, sans-serif;\n  font-size: 14px;\n}\n.code\n{\n  width: 100%;\n  border: 2px solid #49483e;\n  border-radius: 4px;\n}\n\n.cpp_code\n{\n  text-align: left;\n	margin: 0;\n  margin-left: 0px;\n  overflow: auto;\n  padding-left: 1em;\n  padding-bottom: 5px;\n  padding-top: 5px;\n  padding-right: 5px;\n  border-left: 2px solid #49483e;\n}\n\n.num_lines\n{\n  color: #8f908a;\n  float: left;\n  margin: 0px;\n  text-align: right;\n  padding-left: 3px;\n  padding-right: 5px;\n  padding-bottom: 5px;\n  padding-top: 5px;\n}\n\n.p1{color: #a6e22e;}\n.p2{color: #ff9b4b;}\n.p3{color: #f92672;}\n.p4{color: #66d9ef;}\n.p5{color: #B15555;}\n.p6{color: #ae81ff;}\n.p7{color: #e6db74;}\n.p8{color: gray;}\n</style>\n</head>\n<body>\n<div class=\"code\">\n<pre class=\"num_lines\">\n";
+		output << "<html>\n<head>\n<meta charset=\"utf-8\">\n<style>\ndiv:after\n{\n	content: \".\";\n	display: inline-block;\n	clear: both;\n	visibility: hidden;\n	line-height: 0;\n	height: 0;\n}\n\ndiv\n{\n	display: inline-block;\n}\n\nbody\n{\n  background: #272822;\n  color: #f8f8f2;\n  font-family: Helvetica, Arial, sans-serif;\n  font-size: 14px;\n}\n.code\n{\n  width: 100%;\n  border: 2px solid #49483e;\n  border-radius: 4px;\n}\n\n.cpp_code\n{\n  text-align: left;\n	margin: 0;\n  margin-left: 0px;\n  overflow: auto;\n  padding-left: 1em;\n  padding-bottom: 5px;\n  padding-top: 5px;\n  padding-right: 5px;\n  border-left: 2px solid #49483e;\n}\n\n.num_lines\n{\n  color: #8f908a;\n  float: left;\n  margin: 0px;\n  text-align: right;\n  padding-left: 3px;\n  padding-right: 5px;\n  padding-bottom: 5px;\n  padding-top: 5px;\n}\n\n.p1{color: #a6e22e;}\n.p2{color: #ff9b4b;}\n.p3{color: #f92672;}\n.p4{color: #66d9ef;font-style: italic;}\n.p41{color: #66d9ef;}\n.p5{color: #B15555;}\n.p6{color: #ae81ff;}\n.p7{color: #e6db74;}\n.p8{color: gray;}\n</style>\n</head>\n<body>\n<div class=\"code\">\n<pre class=\"num_lines\">\n";
 		if(!file.eof())
 		{
 			getline(file, lol);
